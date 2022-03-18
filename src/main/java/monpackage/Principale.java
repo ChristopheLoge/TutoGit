3b2f3ebd087239ccd99ec1121dc344d2ad4eb22f
ChristopheLoge <christophe.loge@u-picardie.fr>
2022-03-18 13:29:42 +0100

    1ier commite

--- src/main/java/monpackage/Principale.java
+++ src/main/java/monpackage/Principale.java
@@ -3,7 +3,7 @@ package monpackage;
 public class Principale {
 
     public static void main(String[] args) {
-        System.out.println("Bonjour");
+        System.out.println("Bonjour hello ");
 
         /*
         https://www.youtube.com/watch?v=shm5FdsyYts
